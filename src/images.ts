d533a1ebda0a0b3faa417c169b7608c15d9cc9b1
Suyash Dhiman <139106983+SuyashDhiman856@users.noreply.github.com>
2025-08-31 15:07:32 +0530

    Add files via upload

---
+++ src/images.ts
@@ -0,0 +1,43 @@
+import MeriDiya1 from '@/assets/MeriDiya1.jpg';
+import MeriDiya2 from '@/assets/MeriDiya2.jpg';
+import MeriDiya3 from '@/assets/MeriDiya3.jpg';
+import MeriDiya4 from '@/assets/MeriDiya4.jpg';
+import MeriDiya5 from '@/assets/MeriDiya5.jpg';
+import MeriDiya6 from '@/assets/MeriDiya6.jpg';
+import MeriDiya7 from '@/assets/MeriDiya7.jpg';
+import SuyashDiyaFOREVER1 from '@/assets/SuyashDiyaFOREVER1.jpg';
+import SuyashDiyaFOREVER2 from '@/assets/SuyashDiyaFOREVER2.jpg';
+import SuyashDiyaFOREVER3 from '@/assets/SuyashDiyaFOREVER3.jpg';
+import SuyashDiyaFOREVER4 from '@/assets/SuyashDiyaFOREVER4.jpg';
+import SuyashDiyaFOREVER5 from '@/assets/SuyashDiyaFOREVER5.jpg';
+import SuyashDiyaFOREVER6 from '@/assets/SuyashDiyaFOREVER6.jpg';
+import SuyashDiyaFOREVER7 from '@/assets/SuyashDiyaFOREVER7.jpg';
+import SuyashDiyaFOREVER8 from '@/assets/SuyashDiyaFOREVER8.jpg';
+import SuyashDiyaFOREVER9 from '@/assets/SuyashDiyaFOREVER9.jpg';
+import SuyashDiyaFOREVER10 from '@/assets/SuyashDiyaFOREVER10.jpg';
+import SuyashDiyaFOREVER11 from '@/assets/SuyashDiyaFOREVER11.jpg';
+
+const images = {
+images : {
+    MeriDiya1,
+    MeriDiya2,
+    MeriDiya3,
+    MeriDiya4,
+    MeriDiya5,
+    MeriDiya6,
+    MeriDiya7,
+    SuyashDiyaFOREVER1,
+    SuyashDiyaFOREVER2,
+    SuyashDiyaFOREVER3,
+    SuyashDiyaFOREVER4,
+    SuyashDiyaFOREVER5,
+    SuyashDiyaFOREVER6,
+    SuyashDiyaFOREVER7,
+    SuyashDiyaFOREVER8,
+    SuyashDiyaFOREVER9,
+    SuyashDiyaFOREVER10,
+    SuyashDiyaFOREVER11
+}
+};
+
+export default images;
---
+++ src/musics.ts
@@ -0,0 +1,19 @@
+import TumHoTo from '@/assets/Tum Ho To.mp3';
+import ILoveYouDiya from '@/assets/I Love You Diya.mp3';
+import WannaBeYours from '@/assets/I Wanna Be Yours.mp3';
+import Raabta from '@/assets/Raabta.mp3';
+import TumSeHi from '@/assets/Tum Se Hi.mp3';
+import PhirKabhi from '@/assets/Phir Kabhi.mp3';
+
+const musics = {
+    musics : {
+        TumHoTo,
+        ILoveYouDiya,
+        WannaBeYours,
+        Raabta,
+        TumSeHi,
+        PhirKabhi
+    }
+}
+
+export default musics;
