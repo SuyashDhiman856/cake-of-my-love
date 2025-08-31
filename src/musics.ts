282c8eeb770d52b5016e2dcf88670bd5ff4f72a2
Suyash Dhiman <139106983+SuyashDhiman856@users.noreply.github.com>
2025-08-31 21:41:53 +0530

    Update musics.ts

--- src/musics.ts
+++ src/musics.ts
@@ -1,5 +1,5 @@
 import TumHoTo from '@/assets/Tum Ho To.mp3';
-import ILoveYouDiya from '@/assets/I Love You Diya.mp3';
+// import ILoveYouDiya from '@/assets/I Love You Diya.mp3';
 import WannaBeYours from '@/assets/I Wanna Be Yours.mp3';
 import Raabta from '@/assets/Raabta.mp3';
 import TumSeHi from '@/assets/Tum Se Hi.mp3';
@@ -8,12 +8,13 @@ import PhirKabhi from '@/assets/Phir Kabhi.mp3';
 const musics = {
     musics : {
         TumHoTo,
-        ILoveYouDiya,
+        // ILoveYouDiya,
         WannaBeYours,
         Raabta,
         TumSeHi,
         PhirKabhi
     }
 }
+
 
 export default musics;
